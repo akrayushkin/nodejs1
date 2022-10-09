c3c2043a9b6d85877d7e3b1bbf1fbe70ea0c4212
Anton Kraiushkin <Anton_Kraiushkin@epam.com>
2022-10-10 00:43:50 +0500

    Task2

---
+++ task2.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csvtojson');
+const { pipeline } = require('stream');
+
+const csvFilePath = path.join(__dirname, 'csv', 'nodejs-hw1-ex1.csv');
+const jsonFilePath = path.join(__dirname, 'json', 'nodejs-hw1-ex1.json');
+// readFile
+/*
+fs.readFile(
+  csvFilePath,
+  (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(data);
+  }
+)
+ */
+
+// readStream
+const readStream = fs.createReadStream(csvFilePath, { highWaterMark: 10 });
+const writeStream = fs.createWriteStream(jsonFilePath);
+
+/*
+readStream.pipe(csv())
+          .on('error', err => {
+            if (err) {
+              throw err;
+            }
+          })
+          .pipe(writeStream)
+          .on('close', () => {
+            console.log('file was closed');
+          })
+          .on('error', err => {
+            if (err) {
+              throw err;
+            }
+          });
+ */
+
+pipeline(
+  readStream,
+  csv(),
+  writeStream,
+  err => {
+    if (err) {
+      throw err;
+    }
+    console.log('finished');
+  }
+);
